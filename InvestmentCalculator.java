4e1e18307117ef0c0f26b537ad7dcf4c7e56012b
Edmond-1999 <agburugaedmond@gmail.com>
2025-11-09 22:12:54 +0100

    assignment

---
+++ InvestmentCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class InvestmentCalculator {
+public static void main(String[] args) {
+Scanner input = new Scanner(System.in);
+
+System.out.print("Enter investment amount: ");
+double investmentAmount = input.nextDouble();
+
+System.out.print("Enter annual interest rate in percentage: ");
+double annualInterestRate = input.nextDouble();
+
+System.out.print("Enter number of years: ");
+int years = input.nextInt();
+
+double monthlyInterestRate = annualInterestRate / 1200;
+double futureInvestmentValue = investmentAmount * Math.pow(1 + monthlyInterestRate, years * 12);
+
+System.out.println("Future value is $" + futureInvestmentValue);
+}
+}
